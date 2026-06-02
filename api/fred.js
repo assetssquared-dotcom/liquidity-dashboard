0a19e351a7114a077846ca32ac55a1f02cf58517
asset.x2 <assetssquared@gmail.com>
2026-06-02 14:39:38 +0900

    Add files via upload

--- api/fred.js
+++ api/fred.js
@@ -1,6 +1,6 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
+  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=7200');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { series, days } = req.query;
